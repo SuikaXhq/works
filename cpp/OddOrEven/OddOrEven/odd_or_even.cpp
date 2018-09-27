a1fc498d0a8e0be43258b2f98ab314feb0542112
Xhq <xuhaoqing2013@hotmail.com>
2018-09-27 23:50:03 +0800

    Work1

---
+++ cpp/OddOrEven/OddOrEven/odd_or_even.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int i;
+	while (cin >> i) {
+		if (i % 2 == 0)
+	
+			cout << "Even Number\n";
+		else
+			cout << "Odd Number\n";
+	}
+}
